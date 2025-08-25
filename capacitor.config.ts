e3aad1b36f804d1076d39eaafb0f1930eb0b74e1
semtetog <brennodmoraes@gmail.com>
2025-08-25 09:50:04 -0300

    Test: Força o carregamento 100% local desativando o server config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,12 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'br.com.inteligenciatitan.app',
   appName: 'Titan IA',
-  webDir: 'www',
-  // ADICIONE ESTE BLOCO ABAIXO
-  server: {
-    // Esta linha diz ao iOS para não abrir este domínio no Safari.
-    hostname: 'inteligenciatitan.com.br'
-  }
+  webDir: 'www'
+  // Sem o bloco 'server'
 };
 
 export default config;
