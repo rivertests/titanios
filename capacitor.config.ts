eb52e17d0463c1838e8aec7e830acd21fcce120c
semtetog <brennodmoraes@gmail.com>
2025-08-25 08:37:42 -0300

    Commit inicial do projeto Titan IA

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'br.com.inteligenciatitan.app',
+  appName: 'Titan IA',
+  webDir: 'www'
+};
+
+export default config;
