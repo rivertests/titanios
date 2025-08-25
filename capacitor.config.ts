8634833dee9c85b088f397b21578f2593076f75e
semtetog <brennodmoraes@gmail.com>
2025-08-25 10:31:29 -0300

    Finalização: Corrige zoom, piscar, status bar e splash screen

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,15 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'br.com.inteligenciatitan.app',
   appName: 'Titan IA',
-  webDir: 'www',
-  plugins: {
-    StatusBar: {
-      style: 'DARK' // Define os ícones da status bar como brancos
-    },
-    SplashScreen: {
-      launchShowDuration: 0 // Garante que a splash screen fique visível até mandarmos esconder
-    }
-  }
+  webDir: 'www'
+  // Sem o bloco 'server'
 };
 
 export default config;
